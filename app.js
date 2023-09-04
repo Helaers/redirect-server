85230e9b3b2f54b5674729551fbc090447c0522a
Sfen Helaers <sfenhelaers@gmail.com>
2023-09-04 09:34:17 +0200

    chore: lowercase  header key

--- app.js
+++ app.js
@@ -4,23 +4,22 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-	res.send("Server runnning.");
+  res.send("Server runnning.");
 });
 
 app.get("/strobbo-clock", (req, res) => {
-	console.log("Headers: ", req.headers);
-	const authorization = req.headers["Authorization"]?.toLowerCase() ?? ""; // eg "kiosk 1234"
-	const PIN = authorization.replace("kiosk ", "");
+  console.log("Headers: ", req.headers);
+  const authorization = req.headers["authorization"]?.toLowerCase() ?? ""; // eg "kiosk 1234"
+  const PIN = authorization.replace("kiosk ", "");
 
-	const baseUrl =
-		"https://onlinewerkrooster.github.io/strobbo-clock/dev_otomat";
+  const baseUrl = "https://onlinewerkrooster.github.io/strobbo-clock/dev_otomat";
 
-	const url = PIN ? `${baseUrl}?pin=${PIN}` : baseUrl;
+  const url = PIN ? `${baseUrl}?pin=${PIN}` : baseUrl;
 
-	console.log(`Redirecting to: ${url}`);
-	res.redirect(url);
+  console.log(`Redirecting to: ${url}`);
+  res.redirect(url);
 });
 
 app.listen(PORT, () => {
-	console.log(`Server listening at port ${PORT}`);
+  console.log(`Server listening at port ${PORT}`);
 });
